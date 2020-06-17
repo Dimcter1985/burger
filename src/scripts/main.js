bc492cbe8184a6771010db670f70e60af867f764
Dmitriy Rybnikov <senior.ribnickow1985@yandex.ru>
2020-06-17 14:05:58 +0700

    fix mobile scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,124 +1,4 @@
-﻿//  ONE PAGE SCROLL
-const sections = $('.section');
-const display = $('.main');
-let inscroll = false;
-let scrolable = true;
-let pointMenu = $('.point-menu__link');
-
-const mobileDetect = new MobileDetect(window.navigator.userAgent);
-const isMobile = mobileDetect.mobile();
-
-const countPositionPercent = sectionEq => {
-  return `${sectionEq * -100}%`;
-};
-
-const switchActiveClass = (elems, elemNdx) => {
-
-  elems
-    .eq(elemNdx)
-    .addClass('active')
-    .siblings()
-    .removeClass('active');
-
-  //черный цвет для dots
-  if(elemNdx == 1 || elemNdx == 6 || elemNdx == 8) {
-    $(pointMenu).addClass('point-menu__link--black');
-  } else {
-      $(pointMenu).removeClass('point-menu__link--black');
-    };
-};
-
-const unBlockScroll = () => {
-  setTimeout(() => {
-    inscroll = false;
-  }, 600); // подождать пока завершится инерция на тачпадах
-};
-
-const performTransition = sectionEq => {
-  if (inscroll) return;
-  inscroll = true;
-
-  const position = countPositionPercent(sectionEq);
-  const switchFixedMenuClass = () =>
-  switchActiveClass($(pointMenu), sectionEq)
-
-  switchActiveClass(sections, sectionEq);
-  switchFixedMenuClass();
-
-  display.css({
-    transform: `translateY(${position})`
-  });
-  unBlockScroll();
-};
-
-
-const scrollViewport = direction => {
-  const activeSection = sections.filter(".active");
-  const nextSection = activeSection.next();
-  const prevSection = activeSection.prev();
-
-  if (direction === "next" && nextSection.length) {
-    performTransition(nextSection.index());
-  }
-
-  if (direction === "prev" && prevSection.length) {
-    performTransition(prevSection.index());
-  }
-};
-
-
-$(document).on({
-  wheel: e => {
-    const deltaY = e.originalEvent.deltaY;
-    const direction = deltaY > 0 ? "next" : "prev";
-
-    if (scrolable) {
-      scrollViewport(direction);
-    } 
-  },
-  keydown: e => {
-    const tagName = e.target.tagName.toLowerCase();
-    const userTypingInInputs = tagName === "input" || tagName === "textarea";
-
-    if (userTypingInInputs) return;
-
-    switch (e.keyCode) {
-      case 40:
-        scrollViewport("next");
-        break;
-
-      case 38:
-        scrollViewport("prev");
-        break;
-    }
-  }
-});
-
-$("[data-scroll-to]").on("click", e => {
-  e.preventDefault();
-  performTransition(parseInt($(e.currentTarget).attr("data-scroll-to")));
-});
-
-
-// разрешаем свайп на мобильниках
-if (isMobile) {
-//   window.addEventListener(
-//     "touchmove",
-//     e => {
-//       e.preventDefault();
-//     },
-//     { passive: false }
-//   );
-
-  $("body").swipe({
-    swipe: (event, direction) => {
-      let scrollDirecrion;
-      if (direction === "up") scrollDirecrion = "next";
-      if (direction === "down") scrollDirecrion = "prev";
-      scrollViewport(scrollDirecrion);
-    }
-  });
-}
+﻿
 
 
 // ПОЛНОЭКРАННОЕ МЕНЮ
@@ -473,3 +353,125 @@ for (let index = 0; index < coords.length; index++) {
     myMap.setZoom(myMap.getZoom()-1.4);
 };
 ymaps.ready(init);
+
+//  ONE PAGE SCROLL
+const sections = $('.section');
+const display = $('.main');
+let inscroll = false;
+let scrolable = true;
+let pointMenu = $('.point-menu__link');
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+const countPositionPercent = sectionEq => {
+  return `${sectionEq * -100}%`;
+};
+
+const switchActiveClass = (elems, elemNdx) => {
+
+  elems
+    .eq(elemNdx)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+
+  //черный цвет для dots
+  if(elemNdx == 1 || elemNdx == 6 || elemNdx == 8) {
+    $(pointMenu).addClass('point-menu__link--black');
+  } else {
+      $(pointMenu).removeClass('point-menu__link--black');
+    };
+};
+
+const unBlockScroll = () => {
+  setTimeout(() => {
+    inscroll = false;
+  }, 600); // подождать пока завершится инерция на тачпадах
+};
+
+const performTransition = sectionEq => {
+  if (inscroll) return;
+  inscroll = true;
+
+  const position = countPositionPercent(sectionEq);
+  const switchFixedMenuClass = () =>
+  switchActiveClass($(pointMenu), sectionEq)
+
+  switchActiveClass(sections, sectionEq);
+  switchFixedMenuClass();
+
+  display.css({
+    transform: `translateY(${position})`
+  });
+  unBlockScroll();
+};
+
+
+const scrollViewport = direction => {
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+
+  if (direction === "next" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+};
+
+
+$(document).on({
+  wheel: e => {
+    const deltaY = e.originalEvent.deltaY;
+    const direction = deltaY > 0 ? "next" : "prev";
+
+    if (scrolable) {
+      scrollViewport(direction);
+    } 
+  },
+  keydown: e => {
+    const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
+
+    if (userTypingInInputs) return;
+
+    switch (e.keyCode) {
+      case 40:
+        scrollViewport("next");
+        break;
+
+      case 38:
+        scrollViewport("prev");
+        break;
+    }
+  }
+});
+
+$("[data-scroll-to]").on("click", e => {
+  e.preventDefault();
+  performTransition(parseInt($(e.currentTarget).attr("data-scroll-to")));
+});
+
+
+// разрешаем свайп на мобильниках
+if (isMobile) {
+  window.addEventListener(
+    "touchmove",
+    e => {
+      e.preventDefault();
+    },
+    { passive: false }
+  );
+
+  $("body").swipe({
+    swipe: (event, direction) => {
+      let scrollDirecrion;
+      if (direction === "up") scrollDirecrion = "next";
+      if (direction === "down") scrollDirecrion = "prev";
+      scrollViewport(scrollDirecrion);
+    }
+  });
+}
